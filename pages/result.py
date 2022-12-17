a849cd4909fab23c9e2edbe651676d663e1f85ab
mantisHayu <120332799+mantisHayu@users.noreply.github.com>
2022-12-17 14:42:13 +0100

    Locators added for result and search modules

---
+++ pages/result.py
@@ -0,0 +1,27 @@
+"""
+This module contains DuckDuckGoResultPage,
+the page object for the DuckDuckGo search result page.
+"""
+
+from selenium.webdriver.common.by import By
+
+
+class DuckDuckGoResultPage:
+  
+  RESULT_LINK = (By.CSS_SELECTOR, "rpagNum-resNum")
+  SEARCH_INPUT = (By.ID, 'search_form_input_homepage')
+
+  def __init__(self, browser):
+    self.browser = browser
+
+  def result_link_titles(self):
+    # TODO
+    return []
+  
+  def search_input_value(self):
+    # TODO
+    return ""
+
+  def title(self):
+    # TODO
+    return ""
