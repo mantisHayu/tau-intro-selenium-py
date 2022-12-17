bdbfaba06c584e489f50e89bb9560a83ee9102ec
mantisHayu <120332799+mantisHayu@users.noreply.github.com>
2022-12-17 15:57:31 +0100

    Project finished

--- pages/search.py
+++ pages/search.py
@@ -4,19 +4,20 @@ the page object for the DuckDuckGo search page.
 """
 
 from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
 
 
 class DuckDuckGoSearchPage:
   
   SEARCH_INPUT = (By.ID, 'search_form_input_homepage')
+  URL = 'https://www.duckduckgo.com'
 
   def __init__(self, browser):
     self.browser = browser
 
   def load(self):
-    # TODO
-    pass
+    self.browser.get(self.URL)
 
   def search(self, phrase):
-    # TODO
-    pass
+    search_input = self.browser.find_element(*self.SEARCH_INPUT)
+    search_input.send_keys(phrase + Keys.RETURN)
